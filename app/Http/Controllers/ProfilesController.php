5ee3a9d843548ea09fc9aa6b47824e5193fce42a
MariamZayed <mariamzayed91@gmail.com>
2022-02-21 12:46:35 +0200

    ProfileController and routes

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class ProfilesController extends Controller
+{
+    public function index($user)
+    {
+        dd(User::find($user));
+        return view('');
+    }
+}
